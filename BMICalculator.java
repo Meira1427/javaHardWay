78677d6cb6f8c59bd382ea3e37a91bb411a58aac
Meira Pentermann <meira@meirapentermann.com>
2017-05-27 14:25:38 -0600

    Study Drills, Question 2

--- BMICalculator.java
+++ BMICalculator.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 
-// inches and pounds version
+// feet, inches, and pounds version
 
 public class BMICalculator {
 	public static void main (String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		double m, kg, bmi, inch, lbs;
+		double m, kg, bmi, feet, inch, lbs;
 		
-		System.out.print("Your height in inches: ");
+		System.out.print("Your height (feet only): ");
+		feet = keyboard.nextDouble();
+		
+		System.out.print("Your height (inches): ");
 		inch = keyboard.nextDouble();
 		
 		System.out.print("Your weight in pounds: ");
 		lbs = keyboard.nextDouble();
+		
+		inch = inch + (12*feet);
 		
 		m = inch*.0254;
 		kg = lbs*.453592;
