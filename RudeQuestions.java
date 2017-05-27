a92a8badb8439d3b8420da0bca9f2140936bd900
Meira Pentermann <meira@meirapentermann.com>
2017-05-27 12:51:30 -0600

    solving the one word vs entire string problem with nextLine method

--- RudeQuestions.java
+++ RudeQuestions.java
@@ -6,7 +6,8 @@ public class RudeQuestions {
 		int age;
 		double weight, income;
 		
-		Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in); //for Int, Doubles, & Stings
+		Scanner scanner = new Scanner(System.in); //for nextLine() method
 		
 		System.out.print("Hello. What is your name?");
 		name = keyboard.next();
@@ -27,8 +28,8 @@ public class RudeQuestions {
 		System.out.print("Well, thanks for answering my rude questions, ");
 		System.out.println(name + ".");
 		
-		System.out.println("What do you think of my manners?");
-		answer = keyboard.next();
+		System.out.print("What do you think of my manners?");
+		answer = scanner.nextLine(); //using my other input scanner for nextLine
 		System.out.println("I'm sorry to hear that. So you really think I'm " + answer + "?");
 		System.out.println("Nice to meet you too!");
 		
@@ -40,5 +41,10 @@ public class RudeQuestions {
 		entering a formula 47+67. Any number is easy to blow up by entering one letter
 		or a string of letters. The age will fail if I enter a fraction.  The numbers 
 		also appear to blow up if I enter a formula or just a mathematical symbol.*/
+		/* #4. keyboard.next() only grabbed first word of string. nextLine() is required 
+		if user types more than one word. However, it seems that using my scanner called
+		keyboard for nextInt and nextDouble made it fail to wait for response when I 
+		used it later for nextLine, so I declared a new scanner called scanner that I used 
+		only with nextLine().*/
 	}
 }
