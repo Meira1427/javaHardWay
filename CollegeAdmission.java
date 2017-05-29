48ee017f8d2d51f93bc6753eca047808858d8e17
Meira Pentermann <meira@meirapentermann.com>
2017-05-29 15:23:17 -0600

    Study Drills #3 with the expanded else statements

--- CollegeAdmission.java
+++ CollegeAdmission.java
@@ -12,18 +12,16 @@ public class CollegeAdmission {
 		
 		out.print("Admission status: ");
 		
-		// #2. moving the "unlikely" case to near the top, messes up the order
-		
 		if (math >= 790)
 			out.print("CERTAIN ");
-		else if (math >= 390)
-			out.print("UNLIKELY ");
 		else if (math >= 710)
 			out.print("SAFE ");
 		else if (math >= 580)
 			out.print("PROBABLE ");
 		else if (math >= 500)
 			out.print("UNCERTAIN ");
+		else if (math >= 390)
+			out.print("UNLIKELY ");
 		else // below 390
 			out.print("DENIED ");
 			
