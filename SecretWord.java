13583e319c7fcc47d7230323a3cb2540edb1b712
Meira Pentermann <meira@meirapentermann.com>
2017-05-29 13:26:16 -0600

    Exercise 18; further modifications"
    q
    :wq
    wq
    "

--- SecretWord.java
+++ SecretWord.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class SecretWord {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		String secret = "please", guess;
+		String secret = "please";
+		String guess;
+		//prefer these on separate lines
 		
 		System.out.print("What's the secret word? ");
 		guess = keyboard.next();
 		
+		//java is bonus secret word
 		if (guess.equals(secret) || guess.equals("java")) {
 			System.out.println("That's correct!");
 		}
