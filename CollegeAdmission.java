346baf96b3f50532e80b736cb21c8378010bfd09
Meira Pentermann <meira@meirapentermann.com>
2017-05-29 14:52:42 -0600

    Study Drill #1, removing the elses from the else ifs

--- CollegeAdmission.java
+++ CollegeAdmission.java
@@ -12,15 +12,17 @@ public class CollegeAdmission {
 		
 		out.print("Admission status: ");
 		
+		// #1. remove the elses to check behavior.
+		
 		if (math >= 790)
 			out.print("CERTAIN ");
-		else if (math >= 710)
+		if (math >= 710)
 			out.print("SAFE ");
-		else if (math >= 580)
+		if (math >= 580)
 			out.print("PROBABLE ");
-		else if (math >= 500)
+		if (math >= 500)
 			out.print("UNCERTAIN ");
-		else if (math >= 390)
+		if (math >= 390)
 			out.print("UNLIKELY ");
 		else // below 390
 			out.print("DENIED ");
