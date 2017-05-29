484b273b3fd65c959d80d90dd2fa209affb8b2b7
Meira Pentermann <meira@meirapentermann.com>
2017-05-29 13:12:26 -0600

    comment out the interrupting sentence and change age so else statement executes

--- ClubBouncer.java
+++ ClubBouncer.java
@@ -1,17 +1,18 @@
 public class ClubBouncer {
 	public static void main(String[] args) {
-		int age = 22;
+		int age = 19;
 		boolean onGuestList = false;
 		double allure = 7.5;
 		String gender = "F";
 		
 		// #1. Break up the if. . . else with a random print statement
 		// this will not compile
+		// comment out the interrupting sentence
 		
 		if (onGuestList || age >= 21 || (gender.equals("F") && allure >= 8) ){
 			System.out.println("You are allowed to enter the club.");
 			}
-		System.out.println("Study Drill #1 Testing if/else break");
+		//System.out.println("Study Drill #1 Testing if/else break");
 		else {
 			System.out.println("You are not allowed to enter the club.");
 		}
