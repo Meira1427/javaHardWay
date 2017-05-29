0e3d56227236bc8eee556484da0e951d8d312ede
Meira Pentermann <meira@meirapentermann.com>
2017-05-29 13:10:12 -0600

    After Study Drill # 1; This doesn't compile

--- ClubBouncer.java
+++ ClubBouncer.java
@@ -5,9 +5,13 @@ public class ClubBouncer {
 		double allure = 7.5;
 		String gender = "F";
 		
+		// #1. Break up the if. . . else with a random print statement
+		// this will not compile
+		
 		if (onGuestList || age >= 21 || (gender.equals("F") && allure >= 8) ){
 			System.out.println("You are allowed to enter the club.");
 			}
+		System.out.println("Study Drill #1 Testing if/else break");
 		else {
 			System.out.println("You are not allowed to enter the club.");
 		}
