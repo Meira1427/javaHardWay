5b647ea07c456b3bc369e7c9aadfdd9fec963a7f
Meira Pentermann <meira@meirapentermann.com>
2017-05-29 20:48:32 -0600

    Study Drill 1, add a password check

--- EnterPIN.java
+++ EnterPIN.java
@@ -4,11 +4,22 @@ public class EnterPIN {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		int pin, entry;
+		String password;
 		
 		pin = 12345;
 		
 		System.out.println("WELCOME TO TH BANK OF JAVA.");
-		System.out.println("ENTER YOUR PIN: ");
+		System.out.print("ENTER YOUR PASSWORD: ");
+		password = keyboard.next();
+		
+		while (!password.equals("javarocks")) {
+			System.out.println("\nINCORRECT PASSWORD. TRY AGAIN.");
+			System.out.print("ENTER YOUR PASSWORD: ");
+			password = keyboard.next();
+		}
+		
+		System.out.println("\nTHANK YOU.");
+		System.out.print("ENTER YOUR PIN: ");
 		entry = keyboard.nextInt();
 		
 		while (entry != pin) {
