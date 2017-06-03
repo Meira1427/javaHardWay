5ba8162bf458d11a6acf56af5ea32eba60e78b69
Meira Pentermann <meira@meirapentermann.com>
2017-06-02 20:52:29 -0600

    Excercise 39, Study Drill #2, does not compile

--- PasswordDigest.java
+++ PasswordDigest.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import javax.xml.bind.DatatypeConverter;
 
 public class PasswordDigest {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		String pw, hash;
 		
@@ -17,5 +17,19 @@ public class PasswordDigest {
 		
 		System.out.println(hash);
 	
+	/**********************************************************
+	Study Drill #2
+	
+	If I remove "throws Exception" from line 6
+	
+	PasswordDigest.java:10: error: unreported exception NoSuchAlgorithmException; must be 
+	caught or declared to be thrown MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		                                                ^
+	PasswordDigest.java:15: error: unreported exception UnsupportedEncodingException; 
+	must be caught or declared to be thrown digest.update( pw.getBytes("UTF-8") );
+		                          ^
+	2 errors
+	
+	***********************************************************/
 	}
 }
