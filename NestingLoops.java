8a2abdd7a666214a63f87c52404af18c1defee82
Meira Pentermann <meira@meirapentermann.com>
2017-07-13 15:33:34 -0600

    Exercise 46, Study Drill #3, changing to println

--- NestingLoops.java
+++ NestingLoops.java
@@ -1,9 +1,9 @@
 public class NestingLoops {
 	public static void main(String[] args) {
 		// this is #1 - I'll call it "CN"
-		// ********************************************************************
-		// Study Drill 1 - The inner loop changes it's variable faster A1 A2 A3
-		/* Study Drill 2 - switching "c" and "n" loop - Call this "NC"
+		/* ********************************************************************
+		  Study Drill 1 - The inner loop changes it's variable faster A1 A2 A3
+		  Study Drill 2 - switching "c" and "n" loop - Call this "NC"
 		                    inner loop faster, now A1 B1 C1 D1 E1 
 		   ********************************************************************/
 		                    
@@ -16,9 +16,13 @@ public class NestingLoops {
 		System.out.println("\n");
 		
 		// this is #2 - I'll call it "AB"
+		/* **********************************************************************
+		   Study Drill 3 - change to println and then back to plain print
+		   **********************************************************************/
+	
 		for (int a=1; a <= 3; a++) {
 			for (int b=1; b <=3; b++) {
-				System.out.print("(" + a + "," + b + ") ");
+				System.out.println("(" + a + "," + b + ") ");
 			}
 			//code to add
 		}
