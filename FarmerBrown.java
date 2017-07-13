421fed49cd7c04bd6dc3b630352c70256ec4d035
Meira Pentermann <meira@meirapentermann.com>
2017-07-13 16:07:51 -0600

    Exercise 47, Study Drill #1, removing my extra print statement

--- FarmerBrown.java
+++ FarmerBrown.java
@@ -12,7 +12,7 @@ public class FarmerBrown {
 		for ( int s=1; s<=9; s++) {
 			for ( int g=1; g<=25; g++) {
 				for ( int c=1; c<=98; c++) {
-					System.out.println("Testing for sheep = " + s + " goats = " + g + " and chickens = " + c);
+					//System.out.println("Testing for sheep = " + s + " goats = " + g + " and chickens = " + c);
 					if( (s+g+c==100) && ((10.00*s)+(3.5*g)+(0.50*c)==100) ) {
 						System.out.print(s + " sheep, ");
 						System.out.print(g + " goats, and ");
