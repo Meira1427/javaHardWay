c176117f34088748af90a53af28eb1f3bef88a58
Meira Pentermann <meira@meirapentermann.com>
2017-07-13 16:20:45 -0600

    Exercise 48, adding another string array

--- ArrayIntro.java
+++ ArrayIntro.java
@@ -2,9 +2,16 @@ public class ArrayIntro {
 	public static void main(String[] args) {
 		String[] planets = { "Mercury", "Venus", "Earth", "Mars",
 							 "Jupiter", "Saturn", "Uranus", "Neptune" };
+		String[] countries = { "Ireland", "England", "France", "Germany", "Netherlands" };
 		
 		for (String p : planets ) {
 			System.out.println(p + "\t" + p.toUpperCase());
 		}
+		
+		for (String c : countries ) {
+			System.out.print(c + "\t");
+		}
+		
+		System.out.println("");
 	}
 }
