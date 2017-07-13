444109e6352ba9fa449cfe96301b2ac438f94574
Meira Pentermann <meira@meirapentermann.com>
2017-07-13 16:07:00 -0600

    Exercise 47, Study Drill #1, making it more efficient

--- FarmerBrown.java
+++ FarmerBrown.java
@@ -1,8 +1,18 @@
 public class FarmerBrown {
 	public static void main(String[] args) {
-		for ( int s=1; s<=100; s++) {
-			for ( int g=1; g<=100; g++) {
-				for ( int c=1; c<=100; c++) {
+		/*
+		Sheep are $10 each, so we don't have to go any further than 9 attempts,
+		since we need one each of the other animals.
+		We have to have at least one sheep and one chicken, so we can have a maximum of 25 goats
+		This is 100-10.00-0.50 = 89.50 divided by 3.50.
+		We can have a maximum of 98 chickens because we need at least one of each other
+		type of animals and our total number of animals cannot exceed 100.
+		Adding print statement to see all the work the computer is doing
+		*/
+		for ( int s=1; s<=9; s++) {
+			for ( int g=1; g<=25; g++) {
+				for ( int c=1; c<=98; c++) {
+					System.out.println("Testing for sheep = " + s + " goats = " + g + " and chickens = " + c);
 					if( (s+g+c==100) && ((10.00*s)+(3.5*g)+(0.50*c)==100) ) {
 						System.out.print(s + " sheep, ");
 						System.out.print(g + " goats, and ");
