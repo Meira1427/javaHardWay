717a45320d5df243e7490f06bf68fbb538f018bf
Meira Pentermann <meira@meirapentermann.com>
2017-07-13 11:30:08 -0600

    Exercise 46, Study Drill #1 & #2

--- NestingLoops.java
+++ NestingLoops.java
@@ -1,8 +1,14 @@
 public class NestingLoops {
 	public static void main(String[] args) {
 		// this is #1 - I'll call it "CN"
-		for (char c='A'; c<= 'E'; c++) {
-			for (int n=1; n <=3; n++) {
+		// ********************************************************************
+		// Study Drill 1 - The inner loop changes it's variable faster A1 A2 A3
+		/* Study Drill 2 - switching "c" and "n" loop - Call this "NC"
+		                    inner loop faster, now A1 B1 C1 D1 E1 
+		   ********************************************************************/
+		                    
+		for (int n=1; n <=3; n++) {
+			for (char c='A'; c<= 'E'; c++) {
 				System.out.println(c + " " + n);
 			}
 		}
