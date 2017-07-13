8a41421edeff7182c4f08ee156991e3a71edc000
Meira Pentermann <meira@meirapentermann.com>
2017-07-13 16:24:14 -0600

    Exercise 48, try indexing the array instead

--- ArrayIntro.java
+++ ArrayIntro.java
@@ -4,8 +4,8 @@ public class ArrayIntro {
 							 "Jupiter", "Saturn", "Uranus", "Neptune" };
 		String[] countries = { "Ireland", "England", "France", "Germany", "Netherlands" };
 		
-		for (String p : planets ) {
-			System.out.println(p + "\t" + p.toUpperCase());
+		for (int i=0; i<planets.length; i++) {
+			System.out.println("At index " + i + " we have " + planets[i]);
 		}
 		
 		for (String c : countries ) {
