fdad88bbb9aff13a7e9ae594e110c6f2cdc9af75
Meira Pentermann <meira@meirapentermann.com>
2017-07-14 11:23:26 -0600

    Exercise 50, Study Drill 1, print count instead

--- ItemNotFound.java
+++ ItemNotFound.java
@@ -5,12 +5,13 @@ public class ItemNotFound {
 		Scanner keyboard = new Scanner(System.in);
 		
 		String[] heroes = {
-			"Abderus", "Achilles", "Aeneas", "Ajax", "Amphitryon", "Bellerophon",
+			"Abderus", "Achilles",  "Hector", "Aeneas", "Ajax", "Amphitryon", "Bellerophon",
 			"Castor", "Chrysippus", "Daedalus", "Diomedes", "Eleusis", "Eunostus",
 			"Ganymede", "Hector", "Iolaus", "Jason", "Meleager", "Odysseus", 
-			"Orpheus", "Perseus", "Theseus" };
+			"Orpheus", "Perseus", "Theseus", "Hector" };
 		String guess;
 		// Study Drill # 1: Change it to a count instead of a boolean
+		// Print number of matches outside of for loop
 		int count = 0;
 		
 		System.out.println("Pop Quiz!");
@@ -19,12 +20,14 @@ public class ItemNotFound {
 		
 		for (String hero : heroes) {
 			if (guess.equals(hero)) {
-				System.out.println("That's one of them!");
 				count++;
 			}
 		}
 		
-		if (count == 0) {
+		if (count > 0) {
+			System.out.println("Found " + count + " match(es)");
+		}
+		else {
 			System.out.println("No, " + guess + " wasn't a Greek mortal hero.");
 		}
 	}	
