3bdd8d08372df1e26b71f636279c24c918c681e1
Meira Pentermann <meira@meirapentermann.com>
2017-07-25 12:44:56 -0600

    Exercise 55, Study Drill #2, Add new student, make his-her GPA higher than Dave

--- StudentDatabase.java
+++ StudentDatabase.java
@@ -25,12 +25,19 @@ public class StudentDatabase {
 		db[1] = new Student();
 		db[1].name = "Dave";
 		db[1].credits = 15;
-		db[1].gpa = 4.0;
+		db[1].gpa = 3.9;
 		
 		db[2] = new Student();
 		db[2].name = "Michelle";
 		db[2].credits = 132;
 		db[2].gpa = 3.72;
+		
+		db[3] = new Student();
+		db[3].name = "Anna";
+		db[3].credits = 59;
+		db[3].gpa = 4.0;
+		
+		
 		
 		for (int i=0; i<db.length; i++) {
 			System.out.println("Name: " + db[i].name);
