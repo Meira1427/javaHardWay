a09345cfe729c82365753df46b0801fec2b0b464
Meira Pentermann <meira@meirapentermann.com>
2017-07-26 17:31:59 -0600

    Exercise 56, Study Drill #2, Calculate High in Feb and overall High

--- TemperaturesByDate2.java
+++ TemperaturesByDate2.java
@@ -30,19 +30,39 @@ public class TemperaturesByDate2 {
 		
 		System.out.println(numRecords + " daily temperatures loaded.");
 		
-		double total = 0, avg;
-		int count = 0;
+		double totalNov = 0;
+		double totalFeb = 0;
+		double avgNov = 0;
+		double highFeb = 0;
+		double highTemp = 0;
+		int countNov = 0;
+		int countFeb = 0;
 		for (i=0; i<numRecords; i++) {
+			if (tempDB[i].temperature > highTemp) {
+				highTemp = tempDB[i].temperature;
+			}
 			if (tempDB[i].month == 11) {
-				total += tempDB[i].temperature;
-				count++;
+				totalNov += tempDB[i].temperature;
+				countNov++;
+			}
+			if (tempDB[i].month == 2) {
+				if (tempDB[i].temperature > highFeb) {
+					highFeb = tempDB[i].temperature;
+				}
+				countFeb++;
 			}
 		}
 		
-		avg = total/count;
-		avg = roundToOneDecimal(avg);
-		System.out.print("Average daily temperature over " + count);
-		System.out.println(" days in November: " + avg);
+		avgNov = totalNov/countNov;
+		avgNov = roundToOneDecimal(avgNov);
+		System.out.print("Average daily temperature over " + countNov);
+		System.out.println(" days in November: " + avgNov);
+		System.out.print("Highest daily average temperature over " + countFeb);
+		System.out.println(" days in February: " + highFeb);
+		System.out.print("Highest daily avg temp over period from ");
+		System.out.print(tempDB[0].month + "-" + tempDB[0].day + "-" + tempDB[0].year + " to ");
+		System.out.print(tempDB[numRecords-1].month + "-" + tempDB[numRecords-1].day + "-"); 
+		System.out.println(tempDB[numRecords-1].year + " was " + highTemp);
 
 	}
 	
